136efd3d2ce9247c491e99f75ad6816200b05195
DaniyalKukda <daniyalkukda545@gmail.com>
2019-06-21 18:25:11 +0500

    detail view ui complete

---
+++ src/components/detailviewcard/detailviewcard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Card, Button, Avatar } from 'antd';
+
+const { Meta } = Card;
+
+export default function detailviewcard(props) {
+    return (
+        <Card
+            style={{ width: 300 }}
+            cover={
+                <img
+                    alt="example"
+                    src={props.picture}
+                    className="card-img"
+                />
+            }
+            actions={[<Button size={"medium"}>
+                Order Now!
+        </Button>]}
+        >
+            <Meta
+                avatar={<Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>{props.name.substr(0,1)}</Avatar>}
+                title={props.name}
+                description={props.description}
+            />
+        </Card>
+    );
+}
